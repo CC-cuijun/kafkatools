22124ba9af20dcf14ab434c98353a3a8322297ae
CC <51074375+312526353@users.noreply.github.com>
2020-06-23 22:22:53 +0800

    Update run_kafka.sh

--- shell/run_kafka.sh
+++ shell/run_kafka.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 # Create by cuijun 2015-12-24
 # Update 2016-03-24
+# update 2020-06-23 增加注释，删除一些不再使用都方法
  
+# 配置kafka路径
 kafka_dir=/opt/kafka
+# kafka配置
 kafka_conf=$kafka_dir/config/server.properties 
+# kafka broker配置
 kafka_broker_list=127.0.0.1:9092,10.11.12.13:9092,10.11.12.13:9092
 
+# 日志文件路径
 logdir=/opt/logs/kafka
+# zk地址
 zookeeper=127.0.0.1:2181
+# 配置partition
 partition=4
 replication=1
 
+# 使用说明
 Usage(){
         echo Usage:
-        echo "$0 start|stop|restart|status|start-manager"
+        echo "$0 start|stop|restart|status"
         echo "$0 list|conlist"
         echo "$0 consumerstatus topicname consumer"
         echo "$0 watch|send|create|delete topicname"
@@ -21,10 +29,6 @@ Usage(){
 
 Start(){
      $kafka_dir/bin/kafka-server-start.sh $kafka_conf >> $logdir/kafka_start.log  2>&1 &
-}
-
-StartManager(){
-    /home/cuijun/tools/kafka-manager-1.3.0.7/bin/kafka-manager -Dconfig.file=/home/cuijun/tools/kafka-manager-1.3.0.7/conf/application.conf >/dev/null 2>&1 &
 }
 
 Stop(){
@@ -60,6 +64,7 @@ consumerstatus(){
     $kafka_dir/bin/kafka-run-class.sh kafka.tools.ConsumerOffsetChecker --zookeeper "$zookeeper" --group "$consumer" --topic "$TopicName"
 }
 
+# 若需要重头消费去掉--from-beginnin前得'#'
 watch(){
        $kafka_dir/bin/kafka-console-consumer.sh --zookeeper "$zookeeper" --topic "$TopicName" #--from-beginnin
 } 
@@ -81,9 +86,6 @@ consumer="$3"
 case $OPT in
     start)
         Start
-    ;;
-    start\-manager)
-        StartManager
     ;;
     stop)
         Stop "$2"
